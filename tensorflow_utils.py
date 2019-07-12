ed28b6889217a4851448cc99415cc032a72cd714
Hanbearhug <38373160+Hanbearhug@users.noreply.github.com>
2019-07-12 14:28:14 +0800

    Update tensorflow_utils.py

--- tensorflow_utils.py
+++ tensorflow_utils.py
@@ -4,6 +4,7 @@
 1.array_ops.slice：在指定起点（begin）截取指定大小的张量，size指在每一个维度上
 分别截取的长度。
 2. array_ops.stop_gradient：源码未找到，用于停止对于变量input的梯度计算。
+3. array_ops.stack：在指定维度上合并张量。源码会自动进行格式转换。
 """
 def slice(input_, begin, size, name=None):
   # pylint: disable=redefined-builtin
@@ -42,4 +43,48 @@ def slice(input_, begin, size, name=None):
   """
   return gen_array_ops._slice(input_, begin, size, name=name)
 
+def stack(values, axis=0, name="stack"):
+  """Stacks a list of rank-`R` tensors into one rank-`(R+1)` tensor.
+  Packs the list of tensors in `values` into a tensor with rank one higher than
+  each tensor in `values`, by packing them along the `axis` dimension.
+  Given a list of length `N` of tensors of shape `(A, B, C)`;
+  if `axis == 0` then the `output` tensor will have the shape `(N, A, B, C)`.
+  if `axis == 1` then the `output` tensor will have the shape `(A, N, B, C)`.
+  Etc.
+  For example:
+  ```python
+  x = tf.constant([1, 4])
+  y = tf.constant([2, 5])
+  z = tf.constant([3, 6])
+  tf.stack([x, y, z])  # [[1, 4], [2, 5], [3, 6]] (Pack along first dim.)
+  tf.stack([x, y, z], axis=1)  # [[1, 2, 3], [4, 5, 6]]
+  ```
+  This is the opposite of unstack.  The numpy equivalent is
+  ```python
+  tf.stack([x, y, z]) = np.stack([x, y, z])
+  ```
+  Args:
+    values: A list of `Tensor` objects with the same shape and type.
+    axis: An `int`. The axis to stack along. Defaults to the first dimension.
+      Negative values wrap around, so the valid range is `[-(R+1), R+1)`.
+    name: A name for this operation (optional).
+  Returns:
+    output: A stacked `Tensor` with the same type as `values`.
+  Raises:
+    ValueError: If `axis` is out of the range [-(R+1), R+1).
+  """
+  if axis == 0:
+    try:
+      # If the input is a constant list, it can be converted to a constant op
+      return ops.convert_to_tensor(values, name=name)
+    except (TypeError, ValueError):
+      pass  # Input list contains non-constant tensors
 
+  value_shape = ops.convert_to_tensor(values[0], name=name)._shape_tuple()  # pylint: disable=protected-access
+  if value_shape is not None:
+    expanded_num_dims = len(value_shape) + 1
+    if axis < -expanded_num_dims or axis >= expanded_num_dims:
+      raise ValueError("axis = %d not in [%d, %d)" %
+                       (axis, -expanded_num_dims, expanded_num_dims))
+
+  return gen_array_ops.pack(values, axis=axis, name=name)
